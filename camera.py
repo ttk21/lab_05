2b985192d0912d57639e2be8e24796410802c5e2
Trym Haavardsholm <trym.haavardsholm@ffi.no>
2020-10-11 19:02:01 +0200

    Refactorised ex 1

--- camera.py
+++ camera.py
@@ -5,13 +5,12 @@ from pylie import SO3, SE3
 class PerspectiveCamera:
     """Camera model for the perspective camera"""
 
-    def __init__(self, K: np.ndarray, pose_w_c: SE3):
-        self.K = K
-        self.pose_w_c = pose_w_c
-        self.pose_c_w = pose_w_c.inverse()
+    def __init__(self, f: np.ndarray, c: np.ndarray):
+        self.f = f
+        self.c = c
 
-    @classmethod
-    def looks_at(cls, K: np.ndarray, camera_pos_w: np.ndarray, target_pos_w: np.ndarray, up_vector_w: np.ndarray):
+    @staticmethod
+    def looks_at_pose(camera_pos_w: np.ndarray, target_pos_w: np.ndarray, up_vector_w: np.ndarray):
         cam_to_target_w = target_pos_w - camera_pos_w
         cam_z_w = cam_to_target_w.flatten() / np.linalg.norm(cam_to_target_w)
 
@@ -20,52 +19,61 @@ class PerspectiveCamera:
 
         cam_y_w = np.cross(cam_z_w, cam_x_w)
 
-        pose_w_c = SE3((SO3(np.vstack((cam_x_w, cam_y_w, cam_z_w)).T), camera_pos_w))
+        return SE3((SO3(np.vstack((cam_x_w, cam_y_w, cam_z_w)).T), camera_pos_w))
 
-        return cls(K, pose_w_c)
-
-    def camera_to_world(self, x_c: np.ndarray):
-        return self.pose_w_c * x_c
-
-    def world_to_camera(self, x_w: np.ndarray):
-        return self.pose_c_w * x_w
-
-    def project_to_normalised_3d(self, x_w: np.ndarray):
-        x_c = self.world_to_camera(x_w)
+    @staticmethod
+    def project_to_normalised_3d(x_c: np.ndarray):
         return x_c / x_c[-1]
 
-    def project_to_normalised(self, x_w: np.ndarray):
-        x_n = self.project_to_normalised_3d(x_w)
-        return x_n[:2]
+    @classmethod
+    def project_to_normalised(cls, x_c: np.ndarray):
+        xn = cls.project_to_normalised_3d(x_c)
+        return xn[:2]
 
-    def project_to_pixel(self, x_w: np.ndarray):
-        u = self.K * self.project_to_normalised_3d(x_w)
-        return u[:2]
+    def K(self):
+        K = np.identity(3)
+        np.fill_diagonal(K[:2, :2], self.f)
+        K[:2, [2]] = self.c
+        return K
 
-    def reprojection_error_normalised(self, x_w: np.ndarray, measured_x_n: np.ndarray):
-        return measured_x_n[:2] - self.project_to_normalised(x_w)
+    def project_to_pixel(self, x_c: np.ndarray):
+        return (self.project_to_normalised(x_c) * self.f) + self.c
 
-    def reprojection_error_pixel(self, x_w: np.ndarray, measured_u: np.ndarray):
-        return measured_u - self.project_to_pixel(x_w)
+    def pixel_to_normalised(self, u: np.ndarray):
+        return (u - self.c) / self.f
 
-    def jac_project_normalised_wrt_x_c(self, x_c: np.ndarray):
+    def pixel_cov_to_normalised_com(self, pixel_cov : np.ndarray):
+        S_f = np.diag(1/self.f.flatten())
+        return S_f @ pixel_cov @ S_f.T
+
+    @classmethod
+    def reprojection_error_normalised(cls, x_c: np.ndarray, measured_x_n: np.ndarray):
+        return measured_x_n[:2] - cls.project_to_normalised(x_c)
+
+    def reprojection_error_pixel(self, x_c: np.ndarray, measured_u: np.ndarray):
+        return measured_u - self.project_to_pixel(x_c)
+
+    @staticmethod
+    def jac_project_normalised_wrt_x_c(x_c: np.ndarray):
         x_c = x_c.flatten()
         d = 1 / x_c[-1]
-        x_n = d * x_c
+        xn = d * x_c
 
-        return np.array([[d, 0, -d * x_n[0]],
-                         [0, d, -d * x_n[1]]])
+        return np.array([[d, 0, -d * xn[0]],
+                         [0, d, -d * xn[1]]])
 
-    def jac_project_normalised_wrt_pose_w_c(self, x_w: np.ndarray):
-        x_c = self.world_to_camera(x_w).flatten()
+    @staticmethod
+    def jac_project_world_to_normalised_wrt_pose_w_c(pose_c_w: SE3, x_w: np.ndarray):
+        x_c = (pose_c_w * x_w).flatten()
 
         d = 1 / x_c[-1]
-        x_n = d * x_c
+        xn = d * x_c
 
-        # Corresponds to self.jac_project_normalised_wrt_x_c(self.world_to_camera(x_w)) @ \
-        #                self.pose_c_w.jac_action_Xx_wrt_X(x_w) @ pose.w_c.jac_inverse_X_wrt_X()
-        return np.array([[-d, 0, d * x_n[0], x_n[0] * x_n[1],  -1 - x_n[0] ** 2,  x_n[1]],
-                         [0, -d, d * x_n[1], 1 + x_n[1] ** 2, -x_n[0] * x_n[1], -x_n[0]]])
+        # Corresponds to PerspectiveCamera.jac_project_normalised_wrt_x_c(pose_c_w * x_w) @ \
+        #                pose_c_w.jac_action_Xx_wrt_X(x_w) @ pose_c_w.inverse().jac_inverse_X_wrt_X()
+        return np.array([[-d, 0, d * xn[0], xn[0] * xn[1],  -1 - xn[0] ** 2,  xn[1]],
+                         [0, -d, d * xn[1], 1 + xn[1] ** 2, -xn[0] * xn[1], -xn[0]]])
 
-    def jac_project_normalised_wrt_x_w(self, x_w: np.ndarray):
-        return self.jac_project_normalised_wrt_x_c(self.world_to_camera(x_w)) @ self.pose_c_w.jac_action_Xx_wrt_x()
+    @classmethod
+    def jac_project_world_to_normalised_wrt_x_w(cls, pose_c_w: SE3, x_w: np.ndarray):
+        return cls.jac_project_normalised_wrt_x_c(pose_c_w * x_w) @ pose_c_w.jac_action_Xx_wrt_x()
--- ex_1_motion_only_ba.py
+++ ex_1_motion_only_ba.py
@@ -3,48 +3,67 @@ import visgeom as vg
 import matplotlib
 import matplotlib.pyplot as plt
 import scipy.linalg
-from pylie import SO3, SE3
+from pylie import SE3
 from optim import levenberg_marquardt
 from camera import PerspectiveCamera
 
 """Example 1 - Motion-only Bundle Adjustment"""
 
-class SimpleMotionOnlyBAObjective:
+class PrecalibratedCameraMeasurements:
+    def __init__(self, camera, u, covs_u, x_w):
+        self.camera = camera
+        self.x_w = x_w
+
+        # Transform to the normalised image plane.
+        self.xn = camera.pixel_to_normalised(u)
+
+        # Propagate uncertainty, and precompute square root of information matrices.
+        self.num = u.shape[1]
+        self.covs = [np.identity(2)] * self.num
+        self.sqrt_inv_covs = [np.identity(2)] * self.num
+        for c in range(self.num):
+            self.covs[c] = self.camera.pixel_cov_to_normalised_com(covs_u[c])
+            self.sqrt_inv_covs[c] = scipy.linalg.sqrtm(scipy.linalg.inv(self.covs[c]))
+
+
+
+class PrecalibratedMotionOnlyBAObjective:
     """Implements linearisation of motion-only BA objective function"""
 
-    def __init__(self, K, x_w, x_n, covs):
-        if x_w.shape[0] != 3 or x_n.shape[0] != 2 or x_w.shape[1] != x_n.shape[1]:
-            raise TypeError('Matrices with corresponding points must have same size')
+    def __init__(self, measurement):
+        self.measurement = measurement
 
-        self.K = K
-        self.x_w = x_w
-        self.x_n = x_n
-        self.num_points = x_w.shape[1]
-
-        if len(covs) != self.num_points:
-            raise TypeError('Must have a covariance matrix for each point observation')
-
-        # Compute square root of information matrices.
-        self.sqrt_inv_covs = [None] * self.num_points
-        for i in range(self.num_points):
-            self.sqrt_inv_covs[i] = scipy.linalg.sqrtm(scipy.linalg.inv(covs[i]))
-
-    def linearise(self, T_wc: SE3):
-        cam = PerspectiveCamera(self.K, T_wc)
-
-        A = np.zeros((2 * self.num_points, 6))
-        b = np.zeros((2 * self.num_points, 1))
+    def extract_measurement_jacobian(self, T_cw, measurement):
+        A = np.zeros((2 * measurement.num, 6))
 
         # Enter the submatrices from each measurement:
-        for i in range(self.num_points):
-            A[2 * i:2 * (i + 1), :] = self.sqrt_inv_covs[i] @ \
-                                      cam.jac_project_normalised_wrt_pose_w_c(self.x_w[:, [i]])
-            b[2 * i:2 * (i + 1)] = self.sqrt_inv_covs[i] @ \
-                                   cam.reprojection_error_normalised(self.x_w[:, [i]], self.x_n[:, [i]])
+        for i in range(measurement.num):
+            A[2 * i:2 * (i + 1), :] = \
+                measurement.sqrt_inv_covs[i] @ \
+                measurement.camera.jac_project_world_to_normalised_wrt_pose_w_c(T_cw, measurement.x_w[:, [i]])
+
+        return A
+
+    def extract_measurement_error(self, T_cw, measurement):
+        b = np.zeros((2 * measurement.num, 1))
+
+        # Enter the submatrices from each measurement:
+        for i in range(measurement.num):
+            b[2 * i:2 * (i + 1)] = \
+                measurement.sqrt_inv_covs[i] @ \
+                measurement.camera.reprojection_error_normalised(T_cw * measurement.x_w[:, [i]], measurement.xn[:, [i]])
+
+        return b
+
+    def linearise(self, T_wc: SE3):
+        T_cw = T_wc.inverse()
+
+        A = self.extract_measurement_jacobian(T_cw, self.measurement)
+        b = self.extract_measurement_error(T_cw, self.measurement)
 
         return A, b, b.T.dot(b)
 
-def visualise(true_cam, points_w, observed_x_n, x, cost):
+def visualise(true_pose_w_c, true_box_w, measurement, x, cost):
     # Visualize (press a key to jump to the next iteration).
     # Use Qt 5 backend in visualisation.
     matplotlib.use('qt5agg')
@@ -57,16 +76,19 @@ def visualise(true_cam, points_w, observed_x_n, x, cost):
     ax.set_zlabel('z')
 
     # Plot box and true state
-    vg.plot_pose(ax, true_cam.pose_w_c.to_tuple(), scale=1, alpha=0.4)
-    vg.utils.plot_as_box(ax, points_w)
+    vg.plot_pose(ax, true_pose_w_c.to_tuple(), scale=1, alpha=0.4)
+    vg.utils.plot_as_box(ax, true_box_w)
+
+    # Normalised in 3d.
+    xn_3d = np.vstack((measurement.xn, np.ones((1, measurement.num))))
+
 
     # Plot initial state (to run axis equal first time).
     ax.set_title('Cost: ' + str(cost[0]))
     artists = vg.plot_pose(ax, x[0].to_tuple(), scale=1)
-    curr_cam = PerspectiveCamera(true_cam.K, x[0])
-    artists.extend(vg.plot_camera_image_plane(ax, curr_cam.K, curr_cam.pose_w_c.to_tuple()))
-    artists.extend(vg.utils.plot_as_box(ax, curr_cam.camera_to_world(observed_x_n), alpha=0.4))
-    artists.extend(vg.utils.plot_as_box(ax, curr_cam.camera_to_world(curr_cam.project_to_normalised_3d(points_w))))
+    artists.extend(vg.plot_camera_image_plane(ax, measurement.camera.K(), x[0].to_tuple()))
+    artists.extend(vg.utils.plot_as_box(ax, x[0] * xn_3d, alpha=0.4))
+    artists.extend(vg.utils.plot_as_box(ax, x[0] * measurement.camera.project_to_normalised_3d(x[0].inverse() * measurement.x_w)))
     vg.plot.axis_equal(ax)
     plt.draw()
 
@@ -82,40 +104,51 @@ def visualise(true_cam, points_w, observed_x_n, x, cost):
         ax.set_title('Cost: ' + str(cost[i]))
         artists = vg.plot_pose(ax, x[i].to_tuple(), scale=1)
 
-        curr_cam = PerspectiveCamera(true_cam.K, x[i])
-        artists.extend(vg.plot_camera_image_plane(ax, curr_cam.K, curr_cam.pose_w_c.to_tuple()))
-        artists.extend(vg.utils.plot_as_box(ax, curr_cam.camera_to_world(observed_x_n), alpha=0.4))
-        artists.extend(vg.utils.plot_as_box(ax, curr_cam.camera_to_world(curr_cam.project_to_normalised_3d(points_w))))
+        artists.extend(vg.plot_camera_image_plane(ax, measurement.camera.K(), x[i].to_tuple()))
+        artists.extend(vg.utils.plot_as_box(ax, x[i] * xn_3d, alpha=0.4))
+        artists.extend(vg.utils.plot_as_box(ax, x[i] * measurement.camera.project_to_normalised_3d(x[i].inverse() * measurement.x_w)))
         plt.draw()
         while True:
             if plt.waitforbuttonpress():
                 break
 
 
+def generate_measurements(camera, true_pose_w_c, true_points_w):
+    num_points = true_points_w.shape[1]
+
+    # Generate observations in pixels.
+    u = camera.project_to_pixel(true_pose_w_c.inverse() * true_points_w)
+    covs_u = [np.diag(np.array([2, 2]) ** 2)] * num_points  # Same for all observations.
+
+    # Add noise according to uncertainty.
+    for c in range(num_points):
+        u[:2, [c]] = u[:2, [c]] + np.random.multivariate_normal(np.zeros(2), covs_u[c]).reshape(-1, 1)
+
+    # Construct measurement.
+    return PrecalibratedCameraMeasurements(camera, u, covs_u, true_points_w)
+
 def main():
     # World box.
     points_w = vg.utils.generate_box()
 
-    # True camera.
+    # Define common camera.
     w = 640
     h = 480
-    f = 0.75 * h
-    K = np.array([[f, 0, 0.5*w], [0, f, 0.5*h], [0, 0, 1]])
-    true_cam = PerspectiveCamera.looks_at(K, np.array([[3, -4, 0]]).T, np.zeros((3, 1)), np.array([[0, 0, 1]]).T)
+    focal_lengths = 0.75 * h * np.ones((2, 1))
+    principal_point = 0.5 * np.array([[w, h]]).T
+    camera = PerspectiveCamera(focal_lengths, principal_point)
 
-    # Box observations.
-    observed_x_n = true_cam.project_to_normalised_3d(points_w)
-    num_points = observed_x_n.shape[1]
-    point_covariances = [np.diag(np.array([1e-3, 1e-3]) ** 2)] * num_points
-    for c in range(num_points):
-        observed_x_n[:2, [c]] = observed_x_n[:2, [c]] + \
-                                np.random.multivariate_normal(np.zeros(2), point_covariances[c]).reshape(-1, 1)
+    # Generate a set of camera measurements.
+    true_pose_w_c = PerspectiveCamera.looks_at_pose(np.array([[3, -4, 0]]).T, np.zeros((3, 1)), np.array([[0, 0, 1]]).T)
+    measurement = generate_measurements(camera, true_pose_w_c, points_w)
 
-    # Perturb observer pose and use as initial state.
-    init_pose_wc = true_cam.pose_w_c + 0.3 * np.random.randn(6, 1)
+    # Construct model from measurements.
+    model = PrecalibratedMotionOnlyBAObjective(measurement)
+
+    # Perturb camera pose and use as initial state.
+    init_pose_wc = true_pose_w_c + 0.3 * np.random.randn(6, 1)
 
     # Estimate pose in the world frame from point correspondences.
-    model = SimpleMotionOnlyBAObjective(true_cam.K, points_w, observed_x_n[:2, :], point_covariances)
     x, cost, A, b = levenberg_marquardt(init_pose_wc, model)
     cov_x_final = np.linalg.inv(A.T @ A)
 
@@ -125,7 +158,7 @@ def main():
         print(cov_x_final)
 
     # Visualise
-    visualise(true_cam, points_w, observed_x_n, x, cost)
+    visualise(true_pose_w_c, points_w, measurement, x, cost)
 
 
 if __name__ == "__main__":
